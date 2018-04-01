4ade1af2556cb47c5fc75e9af0d8466962a7181d
mchauhan3 <mohitchauhan@lawn-128-61-48-51.lawn.gatech.edu>
2018-04-01 16:38:11 -0400

    like, not equal

--- server/models/item.js
+++ server/models/item.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
   var Item = sequelize.define('item', {
     ISBN: { type: DataTypes.STRING},
     Name: { type: DataTypes.STRING},
-    Description: { type: DataTypes.STRING}
+    //escription: { type: DataTypes.STRING}
   });
 
   return Item;
